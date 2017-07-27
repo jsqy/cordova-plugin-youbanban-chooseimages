18dbdbb3cc2ea69cea61ff49111069525d7b6aeb
xulei <787667174@qq.com>
2017-07-27 14:50:10 +0800

    update perssion

--- src/android/chooseimages.java
+++ src/android/chooseimages.java
@@ -12,8 +12,11 @@ import com.youbanban.cordova.chooseimages.imageloader.MainActivity;
 import com.youbanban.cordova.chooseimages.imageloader.MyAdapter;
 
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.widget.Toast;
 
 
@@ -23,6 +26,12 @@ public class chooseimages extends CordovaPlugin {
 	public static CallbackContext callbackContext;
 	public static boolean isOver = false;
 	public static int maxSize;
+	
+	private static final int REQUEST_EXTERNAL_STORAGE = 1;  
+	private static String[] PERMISSIONS_STORAGE = {  
+	        Manifest.permission.READ_EXTERNAL_STORAGE,  
+	        Manifest.permission.WRITE_EXTERNAL_STORAGE  
+	};  
 
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext)
     throws JSONException {
@@ -31,6 +40,17 @@ public class chooseimages extends CordovaPlugin {
         if(action.equals("setlocation")){
 
         }else if(action.equals("getCamera")){
+        	// Check if we have write permission  
+        	int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);  
+        	if (permission != PackageManager.PERMISSION_GRANTED) {  
+        	   // We don't have permission so prompt the user  
+        	   ActivityCompat.requestPermissions(  
+        	       activity,  
+        	       PERMISSIONS_STORAGE,  
+        	       REQUEST_EXTERNAL_STORAGE  
+        	     );  
+        	 }  
+        	
         	maxSize = Integer.parseInt(args.getString(0));
            Intent intent = new Intent();
            intent.setClass(activity, MainActivity.class);

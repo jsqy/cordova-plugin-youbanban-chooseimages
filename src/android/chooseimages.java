480d400ce3be54c94c85bcb0626f965d29ef6e85
xulei <787667174@qq.com>
2017-07-27 15:53:25 +0800

    update permission

--- src/android/chooseimages.java
+++ src/android/chooseimages.java
@@ -27,11 +27,11 @@ public class chooseimages extends CordovaPlugin {
 	public static boolean isOver = false;
 	public static int maxSize;
 	
-//	private static final int REQUEST_EXTERNAL_STORAGE = 1;  
-//	private static String[] PERMISSIONS_STORAGE = {  
-//	        Manifest.permission.READ_EXTERNAL_STORAGE,  
-//	        Manifest.permission.WRITE_EXTERNAL_STORAGE  
-//	};  
+	private static final int REQUEST_EXTERNAL_STORAGE = 1;  
+	private static String[] PERMISSIONS_STORAGE = {  
+	        Manifest.permission.READ_EXTERNAL_STORAGE,  
+	        Manifest.permission.WRITE_EXTERNAL_STORAGE  
+	};  
 
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext)
     throws JSONException {
@@ -40,42 +40,43 @@ public class chooseimages extends CordovaPlugin {
         if(action.equals("setlocation")){
 
         }else if(action.equals("getCamera")){
-        	// Check if we have write permission  
-//        	int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);  
-//        	if (permission != PackageManager.PERMISSION_GRANTED) {  
-//        	   // We don't have permission so prompt the user  
-//        	   ActivityCompat.requestPermissions(  
-//        	       activity,  
-//        	       PERMISSIONS_STORAGE,  
-//        	       REQUEST_EXTERNAL_STORAGE  
-//        	     );  
-//        	 }  
-//        	
-        	maxSize = Integer.parseInt(args.getString(0));
-           Intent intent = new Intent();
-           intent.setClass(activity, MainActivity.class);
-           this.cordova.startActivityForResult(this, intent, 1);
-           isOver = false;
-           while(true){
-        	   if(isOver){
-        		   isOver = false;
-        		   //下面三句为cordova插件回调页面的逻辑代码
-                   PluginResult mPlugin = new PluginResult(PluginResult.Status.NO_RESULT);
-                   mPlugin.setKeepCallback(true);
-                   callbackContext.sendPluginResult(mPlugin);
-                   String result = "";
-                   for(int i=0;i<MyAdapter.mSelectedImage.size();i++){
-                	   result = result+MyAdapter.mSelectedImage.get(i);
-                	   if(i < MyAdapter.mSelectedImage.size()-1){
-                		   result = result + ",";
-                	   }
-                   }
-       			   MyAdapter.mSelectedImage = new LinkedList<String>();
-                   callbackContext.success(result);
-               	   return true;
-        	   }
-           }
-
+        //0	 Check if we have write permission  
+        	int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);  
+        	if (permission != PackageManager.PERMISSION_GRANTED) {  
+        	   // We don't have permission so prompt the user  
+        	   ActivityCompat.requestPermissions(  
+        	       activity,  
+        	       PERMISSIONS_STORAGE,  
+        	       REQUEST_EXTERNAL_STORAGE  
+        	     );  
+        	   callbackContext.success("permission");
+           	   return false;
+        	 }else{
+        	    	maxSize = Integer.parseInt(args.getString(0));
+        	           Intent intent = new Intent();
+        	           intent.setClass(activity, MainActivity.class);
+        	           this.cordova.startActivityForResult(this, intent, 1);
+        	           isOver = false;
+        	           while(true){
+        	        	   if(isOver){
+        	        		   isOver = false;
+        	        		   //下面三句为cordova插件回调页面的逻辑代码
+        	                   PluginResult mPlugin = new PluginResult(PluginResult.Status.NO_RESULT);
+        	                   mPlugin.setKeepCallback(true);
+        	                   callbackContext.sendPluginResult(mPlugin);
+        	                   String result = "";
+        	                   for(int i=0;i<MyAdapter.mSelectedImage.size();i++){
+        	                	   result = result+MyAdapter.mSelectedImage.get(i);
+        	                	   if(i < MyAdapter.mSelectedImage.size()-1){
+        	                		   result = result + ",";
+        	                	   }
+        	                   }
+        	       			   MyAdapter.mSelectedImage = new LinkedList<String>();
+        	                   callbackContext.success(result);
+        	               	   return true;
+        	        	   }
+        	           }
+        	 }
 
         }
         return true;

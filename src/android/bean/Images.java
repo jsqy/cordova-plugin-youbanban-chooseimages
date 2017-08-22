faf69a9485cd45d6dba48a7b470cf7428d5cab37
徐雷 <xulei@xuleideMacBook-Pro.local>
2017-08-22 13:16:45 +0800

    test

--- src/android/bean/Images.java
+++ src/android/bean/Images.java
@@ -1,9 +1,56 @@
 package com.youbanban.cordova.chooseimages.bean;
 
+import android.graphics.Bitmap;
+
 public class Images {
-	
+
 	private String path;
 	private int num;
+	private Bitmap bitmap;
+	private Bitmap bitmapBig;
+	private int isDelete;
+
+
+	public Images(String path, int num, Bitmap bitmap, Bitmap bitmapBig,
+			int isDelete) {
+		super();
+		this.path = path;
+		this.num = num;
+		this.bitmap = bitmap;
+		this.bitmapBig = bitmapBig;
+		this.isDelete = isDelete;
+	}
+	public int getIsDelete() {
+		return isDelete;
+	}
+	public void setIsDelete(int isDelete) {
+		this.isDelete = isDelete;
+	}
+	public Images(String path, int num, Bitmap bitmap, Bitmap bitmapBig) {
+		super();
+		this.path = path;
+		this.num = num;
+		this.bitmap = bitmap;
+		this.bitmapBig = bitmapBig;
+	}
+	public Bitmap getBitmapBig() {
+		return bitmapBig;
+	}
+	public void setBitmapBig(Bitmap bitmapBig) {
+		this.bitmapBig = bitmapBig;
+	}
+	public Bitmap getBitmap() {
+		return bitmap;
+	}
+	public void setBitmap(Bitmap bitmap) {
+		this.bitmap = bitmap;
+	}
+	public Images(String path, int num, Bitmap bitmap) {
+		super();
+		this.path = path;
+		this.num = num;
+		this.bitmap = bitmap;
+	}
 	public String getPath() {
 		return path;
 	}

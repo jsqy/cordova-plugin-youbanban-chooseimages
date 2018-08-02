536fc8e332f71ec0958fe9ed7b109686674dde7b
zhangguangyu <zhangguangyu@youbanban.com>
2018-08-02 14:33:00 +0800

    false Permission add intent

--- src/android/chooseimages.java
+++ src/android/chooseimages.java
@@ -11,39 +11,55 @@ import com.youbanban.cordova.chooseimages.imageloader.MyAdapter;
 import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.v4.app.ActivityCompat;
+import android.widget.Toast;
 
 public class chooseimages extends CordovaPlugin {
 	public static Activity activity;
 	public static CallbackContext callbackContext;
 	public static int maxSize;
-	private static final int REQUEST_EXTERNAL_STORAGE = 1;
-	private static String[] PERMISSIONS_STORAGE = {
-	        Manifest.permission.READ_EXTERNAL_STORAGE,
-	        Manifest.permission.WRITE_EXTERNAL_STORAGE
-	};
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext)
+		throws JSONException {
+		activity = this.cordova.getActivity();
+		this.callbackContext = callbackContext;
+		maxSize = args.getInt(0);
+		if(action.equals("getPhotos")){
+			if (initPermission()) {
+				Intent intent = new Intent();
+				intent.setClass(activity, MainActivity.class);
+				this.cordova.startActivityForResult(this, intent, 1);
+			}else{
+				getReadPermission(0);
+			}
+		}
+		return true;
+	}
 
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext)
-    throws JSONException {
-        activity = this.cordova.getActivity();
-        this.callbackContext = callbackContext;
-        if(action.equals("getPhotos")){
-        	int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        	if (permission != PackageManager.PERMISSION_GRANTED) {
-        	   ActivityCompat.requestPermissions(
-        	       activity,PERMISSIONS_STORAGE,REQUEST_EXTERNAL_STORAGE
-        	     );
-           	   return false;
-        	 }else{
-        	    	maxSize = args.getInt(0);
-        	        Intent intent = new Intent();
-        	        intent.setClass(activity, MainActivity.class);
-        	        this.cordova.startActivityForResult(this, intent, 1);
-        	 }
-        }
-        return true;
-    }
+	// 权限回调
+	public void onRequestPermissionResult(int requestCode,
+										  String[] permissions, int[] grantResults) throws JSONException {
+		if (cordova.hasPermission(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+			Intent intent = new Intent();
+			intent.setClass(activity, MainActivity.class);
+			this.cordova.startActivityForResult(this, intent, 1);
+		} else {
+			callbackContext.success("没有权限");
+			Toast.makeText(cordova.getActivity(),"没有文件读取权限",Toast.LENGTH_SHORT);
+		}
+	}
+
+
+	protected void getReadPermission(int requestCode) {
+		cordova.requestPermission(this, requestCode, Manifest.permission.READ_EXTERNAL_STORAGE);
+	}
+
+
+	private Boolean initPermission(){
+		if(cordova.hasPermission(Manifest.permission.READ_EXTERNAL_STORAGE)){
+			return true;
+		}else{
+			return false;
+		}
+	}
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent intent){
